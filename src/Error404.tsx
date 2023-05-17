94ab80bf5b6d2e6a9661c283aa004eacf9e5b55e
Nathalie Vanden Abeele <nath.vandenabeele@gmail.com>
2023-05-17 14:59:45 +0200

    Error404 page

---
+++ src/Error404.tsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+const Error404 = () => {
+    return (
+        <div>
+            <Link to="/">Ramenez-moi à la maison!!</Link>
+        </div>
+    );
+};
+
+export default Error404;
