fe840359c27f4b2b0fc302e8a89cc3d409263e43
Nathalie Vanden Abeele <nath.vandenabeele@gmail.com>
2023-05-17 15:20:13 +0200

    footer

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,7 +1,20 @@
+import { Link } from 'react-router-dom';
+
 const Footer = () => {
     return (
-        <div>
-            Some nice footer here.
+        <div className="border-t border-gray-100 flex flex-col p-8 gap-4">
+            <div className="flex-col flex gap-2">
+                <h3 className="font-bold">Informations</h3>
+                <Link to="/contact">Contactez-nous</Link>
+            <Link to="/about">Qui sommes-nous?</Link>
+            <Link to="/job">Jobs</Link>
+            </div>
+            <div className="flex-col flex gap-2">
+            <h3 className="font-bold">Ressources</h3>
+            <Link to="/contact">Voyager avec Sunny Hill</Link>
+            <Link to="/about">Risques</Link>
+            <Link to="/job">Plus</Link>
+            </div>
         </div>
     );
 };
