2c30ff74120b068ce4c68c10970df83f0dd05de7
Nathalie Vanden Abeele <nath.vandenabeele@gmail.com>
2023-05-19 14:12:59 +0200

    Submit button & Titles components

---
+++ src/Submit.tsx
@@ -0,0 +1,10 @@
+const Submit = () => {
+    return (
+        <>
+        <button type="submit" className="flex bg-red-600 text-white p-4 mt-4 rounded-md md:w-44">Suivant</button>
+  
+        </>
+    );
+};
+
+export default Submit;
---
+++ src/Titres.tsx
@@ -0,0 +1,9 @@
+const Titres = (props:{children:string}) => {
+    return (
+        <h3 className="text-3xl font-bold border-b-4 border-b-red-600 w-4/5 md:w-1/3 ml-7 mt-2 p-4">
+           {props.children} 
+        </h3>
+    );
+};
+
+export default Titres;
