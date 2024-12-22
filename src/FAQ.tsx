3df3e6163148ff5c9851af437524cb23a513fe4a
Nvda <nath.vandenabeele@gmail.com>
2024-12-22 11:00:44 +0100

    fix(package): update and fix vulnerabilities

--- src/FAQ.tsx
+++ src/FAQ.tsx
@@ -1,42 +1,49 @@
 import { Disclosure } from "@headlessui/react";
-import CallToAction from "./CallToAction";
 import { AiFillQuestionCircle } from "react-icons/ai";
+import CallToAction from "./CallToAction";
 
 const QUESTIONS = [
   {
-    question: "En combien de temps peut-on attendre une réponse d'un conseiller?",
-    answer: "Nos conseillers sont relativement occupés, mais la moyenne de réponse est de 5 jours ouvrés. Après complétion du formulaire de contact, vous recevrez une confirmation ainsi qu'un numéro de client.",
+    question:
+      "En combien de temps peut-on attendre une réponse d'un conseiller?",
+    answer:
+      "Nos conseillers sont relativement occupés, mais la moyenne de réponse est de 5 jours ouvrés. Après complétion du formulaire de contact, vous recevrez une confirmation ainsi qu'un numéro de client.",
   },
   {
     question:
       "Je n'ai pas reçu de nouvelles de la part de Sunny Hill, cela fait déjà plus de 5 jours ouvrés. Quelle est la marche à suivre?",
-    answer: "Il est possible que votre message n'ait pas été envoyé correctement ou que nos serveurs l'aient perdu en chemin. Pensez tout de même à vérifier dans vos spams si un message ne s'y est pas perdu. Enfin, si le doute persiste, contactez le service client pour toute information supplémentaire.",
+    answer:
+      "Il est possible que votre message n'ait pas été envoyé correctement ou que nos serveurs l'aient perdu en chemin. Pensez tout de même à vérifier dans vos spams si un message ne s'y est pas perdu. Enfin, si le doute persiste, contactez le service client pour toute information supplémentaire.",
   },
   {
-    question: "J'ai organisé mon voyage, mais j'aimerais l'annuler. Comment m'y prendre ?",
-    answer: "Si vous avez déjà payé votre voyage, rien n'est perdu ! Avec Sunny Hill, vous avez 30 jours pour exercer votre droit de rétractation, à compter du versement de l'acompte. Prenez contact avec nos conseillers ou rendez-vous dans l'une de nos agences pour connaître la marche à suivre.",
+    question:
+      "J'ai organisé mon voyage, mais j'aimerais l'annuler. Comment m'y prendre ?",
+    answer:
+      "Si vous avez déjà payé votre voyage, rien n'est perdu ! Avec Sunny Hill, vous avez 30 jours pour exercer votre droit de rétractation, à compter du versement de l'acompte. Prenez contact avec nos conseillers ou rendez-vous dans l'une de nos agences pour connaître la marche à suivre.",
   },
 ];
 
 const Faq = () => {
-  return (<>
-   <div className="flex flex-col justify-center p-8 gap-8 bg-black bg-faq-background text-white bg-cover h-auto flex-grow lg:h-96">
-            <h1 className="text-4xl font-bold lg:text-7xl lg:text-center">Foire aux questions</h1>
-       </div>
-    <div className="p-8 text-sm flex-col gap-2 flex lg:w-1/2 lg:m-auto">
-        
-      {QUESTIONS.map((a) => (
-        <Disclosure>
-          <Disclosure.Button className="border border-gray-100 p-8 rounded-md shadow-sm border-b-blue-600 border-b-2 lg:text-xl lg:flex lg:items-center lg:gap-8">
-            <AiFillQuestionCircle /> {a.question}
-          </Disclosure.Button>
-          <Disclosure.Panel className="bg-gray-100 p-8  lg:text-xl flex-grow-0">
-            {a.answer}
-          </Disclosure.Panel>
-        </Disclosure>
-      ))}
-      <CallToAction />
-    </div>
+  return (
+    <>
+      <div className="flex flex-col justify-center p-8 gap-8 bg-black bg-faq-background text-white bg-cover h-auto flex-grow lg:h-96">
+        <h1 className="text-4xl font-bold lg:text-7xl lg:text-center">
+          Foire aux questions
+        </h1>
+      </div>
+      <div className="p-8 text-sm flex-col gap-2 flex lg:w-1/2 lg:m-auto">
+        {QUESTIONS.map((a) => (
+          <Disclosure>
+            <Disclosure.Button className="border border-gray-100 p-8 rounded-md shadow-sm border-b-blue-600 border-b-2 lg:text-xl lg:flex lg:items-center lg:gap-8">
+              <AiFillQuestionCircle /> {a.question}
+            </Disclosure.Button>
+            <Disclosure.Panel className="bg-gray-100 p-8  lg:text-xl flex-grow-0">
+              {a.answer}
+            </Disclosure.Panel>
+          </Disclosure>
+        ))}
+        <CallToAction />
+      </div>
     </>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,55 +1,57 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import {createBrowserRouter,createRoutesFromElements,Route,RouterProvider} from 'react-router-dom';
-import About from './About.tsx';
-import Product from './Product.tsx';
-import Greetings from './Greetings.tsx';
-import Form from './Form.tsx';
-import StepOne from './FormSteps/StepOne.tsx';
-import StepTwo from './FormSteps/StepTwo.tsx';
-import StepThree from './FormSteps/StepThree.tsx';
-import Error404 from './Error404.tsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import About from "./About.tsx";
+import App from "./App.tsx";
+import Error404 from "./Error404.tsx";
+import Form from "./Form.tsx";
+import StepOne from "./FormSteps/StepOne.tsx";
+import StepThree from "./FormSteps/StepThree.tsx";
+import StepTwo from "./FormSteps/StepTwo.tsx";
+import Greetings from "./Greetings.tsx";
+import "./index.css";
+import Product from "./Product.tsx";
 
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
-import Faq from './Faq.tsx';
-import StepFour from './FormSteps/StepFour.tsx';
-import Confirm from './Confirm.tsx';
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import Confirm from "./Confirm.tsx";
+import Faq from "./Faq.tsx";
+import StepFour from "./FormSteps/StepFour.tsx";
 
 const client = new ApolloClient({
-  uri: 'https://rickandmortyapi.com/graphql',
+  uri: "https://rickandmortyapi.com/graphql",
   cache: new InMemoryCache(),
-})
-
+});
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
-    <Route path="/" element={<App/>}>
-      <Route index element={<Greetings/>}/>
-      <Route path="/about" element={<About/>}/>
-      <Route path="/product" element={<Product/>}/>
-      <Route path="/faq" element={<Faq/>}/>
-      <Route path="/form" element={<Form/>}>
-        <Route index element={<StepOne/>}/>
-        <Route path="/form/step-two" element={<StepTwo/>}/>
-        <Route path="/form/step-three" element={<StepThree/>}/>
-        <Route path="/form/step-four" element={<StepFour/>}/>
-        <Route path="/form/confirm" element={<Confirm/>}/>
+      <Route path="/" element={<App />}>
+        <Route index element={<Greetings />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/faq" element={<Faq />} />
+        <Route path="/form" element={<Form />}>
+          <Route index element={<StepOne />} />
+          <Route path="/form/step-two" element={<StepTwo />} />
+          <Route path="/form/step-three" element={<StepThree />} />
+          <Route path="/form/step-four" element={<StepFour />} />
+          <Route path="/form/confirm" element={<Confirm />} />
+        </Route>
+        <Route path="*" element={<Error404 />} />
       </Route>
-      <Route path="*" element={<Error404/>}/>
-    </Route>
-    
-    
-    </>
-  )
-)
+    </>,
+  ),
+);
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ApolloProvider client={client}>
-    <RouterProvider router={router}/>
+      <RouterProvider router={router} />
     </ApolloProvider>
   </React.StrictMode>,
-)
+);
